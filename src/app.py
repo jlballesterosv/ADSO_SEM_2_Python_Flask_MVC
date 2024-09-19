eafcfa05be1949acc6cd73e31215185937f7d342
jorgeballeuniandes <j.ballesterosv@uniandes.edu.co>
2024-09-18 19:25:12 -0500

    Clase 003 - Inicio de creacion de modelos

--- src/app.py
+++ src/app.py
@@ -16,3 +16,13 @@ def crear_producto():
 @app.route('/ver_productos')
 def ver_productos():
     return render_template('tabla_productos.html', titulo_pagina = 'Ver Productos')
+
+class Productos:
+    descripcion = 'Carne'
+    valor_unitario = 1000
+    cantidad_stock = 10
+    unidad_medida = 'GRS'
+    
+    def crear_producto(descripcion, valor_unitario, cantidad_stock, unidad_medida):
+        return 'Producto creado correctamente'
+    
