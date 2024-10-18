8bf77adad24549fc9eb234b869a376e8cb329310
Jorge Ballesteros <joba1021@netcracker.com>
2024-10-17 19:38:53 -0500

    Creacion de controladores

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
 from src.models import Base, engine
-from src.models.productos import Productos
-from src.models.categorias import Categorias
+from flask_controller import FlaskControllerRegister
+
 
 app = Flask(__name__)
 
@@ -10,31 +10,11 @@ app.debug = True
 
 Base.metadata.create_all(engine)
 
+register = FlaskControllerRegister(app)
+register.register_package('src.controllers')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
-@app.route('/')
-def index():
-    return render_template('index.html', titulo_pagina = 'Inicio')
-
-@app.route('/crear_producto', methods=['POST','GET'])
-def crear_producto():    
-    if request.method == 'POST':
-        descripcion = request.form.get('descripcion')    
-        valor_unitario = request.form.get('valor_unitario')    
-        unidad_medida = request.form.get('unidad_medida')    
-        cantida_stock = request.form.get('cantida_stock')    
-        categoria = request.form.get('categoria')    
-        producto = Productos(descripcion,valor_unitario,unidad_medida,cantida_stock,categoria)
-        Productos.agregar_producto(producto)
-        return redirect(url_for('ver_productos'))
-    categorias = Categorias.obtener_categorias()
-    return render_template('formulario_crear_producto.html', titulo_pagina = 'Crear Producto', categorias=categorias)
-
-@app.route('/ver_productos')
-def ver_productos():
-    productos = Productos.obtener_productos()
-    return render_template('tabla_productos.html', titulo_pagina = 'Ver Productos', productos=productos)
 
 
-    
---
+++ src/controllers/productos_controller.py
@@ -0,0 +1,30 @@
+from src.app import app
+from flask import render_template, request, redirect, url_for
+from flask_controller import FlaskController
+from src.models.productos import Productos
+from src.models.categorias import Categorias
+
+
+class ProductosController(FlaskController):
+    @app.route('/crear_producto', methods=['POST','GET'])
+    def crear_producto():    
+        if request.method == 'POST':
+            descripcion = request.form.get('descripcion')    
+            valor_unitario = request.form.get('valor_unitario')    
+            unidad_medida = request.form.get('unidad_medida')    
+            cantida_stock = request.form.get('cantida_stock')    
+            categoria = request.form.get('categoria')    
+            producto = Productos(descripcion,valor_unitario,unidad_medida,cantida_stock,categoria)
+            Productos.agregar_producto(producto)
+            return redirect(url_for('ver_productos'))
+        categorias = Categorias.obtener_categorias()
+        return render_template('formulario_crear_producto.html', titulo_pagina = 'Crear Producto', categorias=categorias)
+
+    @app.route('/ver_productos')
+    def ver_productos():
+        productos = Productos.obtener_productos()
+        return render_template('tabla_productos.html', titulo_pagina = 'Ver Productos', productos=productos)
+
+
+        
+
